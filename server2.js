796172d84965490c2a62a7541769b6f1ab72fb32
kamimi01 <awakaru0122@gmail.com>
2019-11-04 12:04:00 +0900

    フォームからデータを受信した時の処理の追加

--- server2.js
+++ server2.js
@@ -2,32 +2,48 @@
 // アクセスしてきたURLによって処理を変える
 // ファイルを読み込むためのモジュールは、fs
 // ejsのモジューるの読み込み
+// フォームからの投稿を処理する、querystring
 var http = require('http'),
     fs = require('fs'),
-    ejs = require('ejs');
+    ejs = require('ejs'),
+    qs = require('querystring');
 var settings = require('./settings');
 var server = http.createServer();
 // リクエストが来る前にテンプレートファイルを読み込む
 // 第一引数：ファイル名、第二引数：文字コード
 // readFileSyncは、ブロッキングな命令（これが終わるまで次の処理がされない）
-var template = fs.readFileSync(__dirname + '/public_html/hello.ejs', 'utf-8');
-// ビュー数（初期化）
-var n = 0;
-server.on('request', function(req,res){
-    // テンプレートの中にデータを入れ込む
-    n ++;
-    var data = ejs.render(template,{
-        title:"hello",
-        content:"<strong>World!</strong>",
-        n : n
-    })
-    // 実行するのは、テキストではなく、htmlなので、text/htmlにする
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+// 投稿を保持する配列
+var posts = [];
+function renderForm(posts, res) {
+    var data = ejs.render(template, {
+        // 投稿の内容
+        posts: posts
+    });
     res.writeHead(200, {'Content-Type':'text/html'});
-    // リクエストしてきたURLが「req.url」で取ることができる
-    // res.write('hello from' + req.url);
-    // 表示させるのは、data
     res.write(data);
     res.end();
+};
+server.on('request', function(req,res){
+    // そのフォームが投稿されたかどうかの場合分け
+    // フォームが投稿された時の方は、ほぼ決まり文句
+    if (req.method === 'POST') {
+        // dataの初期化
+        req.data = "";
+        // フォームからデータが送られてくる時はreadableと言うイベントで取ることができる
+        req.on("readable", function(){
+            // その間に、req.dataに読みこんだデータを追加していく
+            req.data += read.read();
+        });
+        // 全てのデータの受信が終わったら、endと言うイベントにする
+        req.on("end", function() {
+        // 
+        })
+    } else {
+        // 投稿されていない時はフォームを表示する必要がある
+        // 長くなるので別関数にする
+        renderForm(posts,res);
+    }
 });
 server.listen(settings.port, settings.host);
 console.log("server listening...");
