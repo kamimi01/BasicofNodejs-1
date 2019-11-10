80cb939ccd6d3459516a219c4849bfeec27b0643
kamimi01 <awakaru0122@gmail.com>
2019-11-10 10:55:02 +0900

    console.dirの説明を追加

--- mongo.js
+++ mongo.js
@@ -3,6 +3,8 @@ var MongoClient = require('mongodb').MongoClient,
     settings = require('./settings');
 // DBに接続する
 // コールバック関数（もしエラーだったら→console.dir(err)でエラー内容を表示してくれる）
+// console.dir(object{, option})とは、オブジェクトの内容を見やすく表示することができる
+// 参考：https://qiita.com/tadnakam/items/dda690bb184fdc74851f
 MongoClient.connect("mongodb://"+settings.host+"/"+settings.db, function(err, db){
     if(err) { return console.dir(err); };
     console.log("connected to db");
