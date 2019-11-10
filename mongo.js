9f5606a3d21c8f996b166fd3ef79d14e2df6e402
kamimi01 <awakaru0122@gmail.com>
2019-11-10 12:17:35 +0900

    warningへの対応

--- mongo.js
+++ mongo.js
@@ -1,13 +1,19 @@
 // mongoが使えるようにする（モジュールを読み込む）
 var MongoClient = require('mongodb').MongoClient,
     settings = require('./settings');
+
+// dbへの接続オプションを追加
+const connectOption = {
+    useUnifiedTopology: true
+};
 // DBに接続する
 // コールバック関数（もしエラーだったら→console.dir(err)でエラー内容を表示してくれる）
 // console.dir(object{, option})とは、オブジェクトの内容を見やすく表示することができる
 // 参考：https://qiita.com/tadnakam/items/dda690bb184fdc74851f
-MongoClient.connect("mongodb://"+settings.host+"/"+settings.db, function(err, client){
+MongoClient.connect("mongodb://"+settings.host+"/"+settings.db, connectOption, function(err, client){
     // mongodb3以降は、 dbを一度定義しないといけなくなった
     // 公式参考：https://github.com/mongodb/node-mongodb-native/blob/3.0/CHANGES_3.0.0.md
+    // DB名「test」なので、「use test」でコレクションの中身を見ることができる
     var db = client.db('test');
     if(err) { return console.dir(err); };
     console.log("connected to db");
@@ -19,7 +25,7 @@ MongoClient.connect("mongodb://"+settings.host+"/"+settings.db, function(err, cl
             {name: "yuji", socre: 60}
         ];
         // コレクションにデータを入れる
-        collection.insert(docs, function(err, result) {
+        collection.insertOne(docs, function(err, result) {
             console.dir(result);
         });
     })
